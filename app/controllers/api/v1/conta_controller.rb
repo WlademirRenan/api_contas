e2b911c593c3df020faa1bdcc75dcf011a54a3d9
Wlademir Renan dos Santos <wlademir.santos.ext@espm.br>
2018-02-18 18:33:20 -0300

    Ajustando indentaçao e criando rotas e actions para cancelar, bloquear e ativar

--- app/controllers/api/v1/conta_controller.rb
+++ app/controllers/api/v1/conta_controller.rb
@@ -1,50 +1,77 @@
 class Api::V1::ContaController < Api::V1::ApiController
-    before_action :set_conta, only: [:show, :update, :destroy]
-  
-    # GET /api/v1/contas
-    def index
-      @contas = Conta.all
-      render json: @contas
-    end
-  
-    # GET /api/v1/contas/1
-    def show
-      render json: @conta
-    end
-    
-    # POST /api/v1/contas
-    def create
-      @conta = Conta.new(conta_params)
-      if @conta.save
-        render json: @conta, status: :created
-      else
-        render json: @conta.errors, status: :unprocessable_entity
-      end
-    end
-      
-    # PATCH/PUT /api/v1/contas/1
-    def update
-      if @conta.update(conta_params)
-        render json: @conta
-      else
-        render json: @conta.errors, status: :unprocessable_entity
-      end
-    end
-     
-    # DELETE /api/v1/contas/1
-    def destroy
-    #  @conta.destroy
-    end
-     
-    private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_conta
-        @conta = Conta.find(params[:id])
-      end
-   
-      # Only allow a trusted parameter "white list" through.
-      def conta_params
-        params.permit(:conta_pai_id, :nome, :pessoa_id, :data_criacao)
-      end
-    
+  before_action :set_conta, only: [:show, :update, :destroy, :cancelar, :bloquear, :ativar]
+
+  # GET /api/v1/contas
+  def index
+    @contas = Conta.all
+    render json: @contas
   end
+
+  # GET /api/v1/contas/1
+  def show
+    render json: @conta
+  end
+
+  # POST /api/v1/contas
+  def create
+    @conta = Conta.new(conta_params)
+    if @conta.save
+      render json: @conta, status: :created
+    else
+      render json: @conta.errors, status: :unprocessable_entity
+    end
+  end
+ 
+  # PATCH/PUT /api/v1/contas/1
+  def update
+    if @conta.update(conta_params)
+      render json: @conta
+    else
+      render json: @conta.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PUT /api/v1/conta/1/cancelar
+  def cancelar
+    if @conta.cancelar(conta_params)
+      render json: @conta
+    else
+      render json: @conta.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PUT /api/v1/conta/1/bloquear
+  def bloquear
+    if @conta.cancelar(conta_params)
+      render json: @conta
+    else
+      render json: @conta.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PUT /api/v1/conta/1/ativar
+  def ativar
+    if @conta.cancelar(conta_params)
+      render json: @conta
+    else
+      render json: @conta.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /api/v1/contas/1
+  def destroy
+    # @conta.destroy
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_conta
+    @conta = Conta.find(params[:id])
+  end
+ 
+  # Only allow a trusted parameter "white list" through.
+  def conta_params
+    params.permit(:conta_pai_id, :nome, :pessoa_id, :data_criacao)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :pessoa
-      resources :conta
+      resources :conta do
+        put '/:id/cancelar' => 'conta#cancelar', on: :collection
+        put '/:id/bloquear' => 'conta#bloquear', on: :collection
+        put '/:id/ativar' => 'conta#ativar', on: :collection
+      end
     end
   end
 end
