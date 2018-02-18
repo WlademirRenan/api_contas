53cdee08680a8f42f49d961bcd1df9723cfa2d8a
Wlademir Renan dos Santos <wlademir.santos.ext@espm.br>
2018-02-18 15:54:25 -0300

    terminando testes de pessoa

--- app/models/pessoa.rb
+++ app/models/pessoa.rb
@@ -1,7 +1,12 @@
 class Pessoa < ApplicationRecord
+  before_validation :remover_espacos
   validates :class_name, presence: true
-  validates :cnpj, :razao_social, :nome_fantasia, presence: true, if: fisica?
-  validates :cpf, :nome_completo, :data_nascimento, presence: true, if: juridica?
+  validates :cpf, :nome_completo, :data_nascimento, presence: true, if: :fisica?
+  validates_length_of :cpf, minimum: 11, maximum: 11, if: :fisica?
+  validates_length_of :nome_completo, minimum: 3, if: :fisica?
+  validates :cnpj, :razao_social, :nome_fantasia, presence: true, if: :juridica?
+  validates_length_of :cnpj, minimum: 14, maximum: 14, if: :juridica?
+  validates_length_of :razao_social, :nome_fantasia, minimum: 3, if: :juridica?
 
   def fisica?
     class_name.eql? 'Fisica'
@@ -9,5 +14,13 @@ class Pessoa < ApplicationRecord
 
   def juridica?
     class_name.eql? 'Juridica'
+  end
+
+  def remover_espacos
+    cpf.to_s.gsub!(/[^0-9]/, '')#.squish!
+    cnpj.to_s.gsub!(/[^0-9]/, '')
+    nome_completo.to_s.squish!
+    razao_social.to_s.squish!
+    nome_fantasia.to_s.squish!
   end
 end
--- spec/models/pessoa_spec.rb
+++ spec/models/pessoa_spec.rb
@@ -1,15 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe Pessoa, type: :model do
-  it 'fisica deve obrigar cpf'
-  it 'fisica deve obrigar nome completo'
-  it 'nome completo deve ser maior que 3 caracteres'
-  it 'fisica deve obrigar data nascimento'
-  it 'cpf deve conter 11 digitos'
+  before(:each) do
+    @fisica = Pessoa.new(class_name: 'Fisica', cpf: '01234567890', nome_completo: 'Fulano de Tal', data_nascimento: '01/01/2000')
+    @juridica = Pessoa.new(class_name: 'Juridica', cnpj: '01234567890123', nome_fantasia: 'Fulano Corp', razao_social: 'Fulano LTDA')
+  end
+  it 'fisica deve obrigar cpf' do
+    @fisica.cpf = nil
+    expect(@fisica).to_not be_valid
+  end
+  it 'fisica deve obrigar nome completo' do
+    @fisica.nome_completo = nil
+    expect(@fisica).to_not be_valid
+  end
+  it 'nome completo deve ser maior que 3 caracteres sem contar espaços' do
+    @fisica.nome_completo = 'a            '
+    expect(@fisica).to_not be_valid
+  end
+  it 'nome completo deve ser maior que 3 caracteres' do
+    @fisica.nome_completo = 'ab'
+    expect(@fisica).to_not be_valid
+  end
+  it 'fisica deve obrigar data nascimento' do
+    @fisica.data_nascimento = nil
+    expect(@fisica).to_not be_valid
+  end
+  it 'cpf deve conter 11 digitos' do
+    @fisica.cpf = '12345678'
+    expect(@fisica).to_not be_valid
+  end
+  it 'cpf deve conter 11 digitos sem contar espaços' do
+    @fisica.cpf = '0123456789 '
+    expect(@fisica).to_not be_valid
+  end
 
-  it 'juridica deve obrigar cnpj'
-  it 'juridica deve obrigar razao social'
-  it 'razao social deve ser maior que 3 caracteres'
-  it 'juridica deve obrigar nome fantasia'
-  it 'nome fantasia dee ser maior que 3 caracteres'
+  it 'juridica deve obrigar cnpj' do
+    @juridica.cnpj = nil
+    expect(@juridica).to_not be_valid
+  end
+  it 'juridica deve obrigar razao social' do
+    @juridica.razao_social = nil
+    expect(@juridica).to_not be_valid
+  end
+  it 'razao social deve ser maior que 3 caracteres' do
+    @juridica.razao_social = 'a       '
+    expect(@juridica).to_not be_valid
+  end
+  it 'juridica deve obrigar nome fantasia' do
+    @juridica.nome_fantasia = nil
+    expect(@juridica).to_not be_valid
+  end
+  it 'nome fantasia dee ser maior que 3 caracteres' do
+    @juridica.nome_fantasia = 'a         '
+    expect(@juridica).to_not be_valid
+  end
+  it 'fisica deve ser valida' do
+    expect(@fisica).to be_valid
+  end
+  it 'juridica deve ser valida' do
+    expect(@juridica).to be_valid
+  end
 end
