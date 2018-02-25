f3e3465b370a0ecb685f82370b245533bd623a6c
Wlademir Renan dos Santos <wlademir.santos.ext@espm.br>
2018-02-25 16:41:33 -0300

    iniciando estorno de aporte

--- app/models/aporte.rb
+++ app/models/aporte.rb
@@ -3,7 +3,7 @@ class Aporte < ApplicationRecord
 
   validates :conta_destino_id, :valor, :token, presence: true
   validates :tipo, inclusion: { in: [true, false] }
-
+  validates :token, uniqueness: true
   validate :transacao_permitida?
 
   before_save :movimentar
@@ -11,6 +11,7 @@ class Aporte < ApplicationRecord
   def transacao_permitida?
     errors.add(:conta_destino_id, 'Filial apenas recebe transferencia') if conta_destino_filial?
     errors.add(:base, 'Não são permitidas operações entre contas canceladas ou bloqueadas') unless transacao_entre_contas_validas?
+    errors.add(:token, 'não é o mesmo da operação original') if token_diferente?
   end
 
   def conta_destino_filial?
@@ -39,5 +40,15 @@ class Aporte < ApplicationRecord
 
   def estornar(valor)
     conta_destino.debitar(valor)
+  end
+
+  def token_diferente?
+    return false if entrada?
+    result = false
+    if token
+      original = Transferencia.where(token: token).first
+      result = token != original.token
+    end
+    result
   end
 end
